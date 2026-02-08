7cb253e3fea3f1ae622313a683f837bf33a2a2ad
harizm837-dot <harizm837@gmail.com>
2026-02-08 15:14:55 +0800

    Update sound.js

--- js/sound.js
+++ js/sound.js
@@ -1,10 +1,3 @@
-const snd = new Audio("https://actions.google.com/sounds/v1/cartoon/clang_and_wobble.ogg");
-
-function playGift(){
-  snd.currentTime=0;
-  snd.play();
-}
-
-function toggleSound(){
-  snd.muted=!snd.muted;
-}
+const snd=new Audio("https://assets.mixkit.co/sfx/preview/mixkit-game-bonus-reached-2065.mp3");
+function playGift(){snd.currentTime=0;snd.play()}
+function toggleSound(){snd.muted=!snd.muted}
