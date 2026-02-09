cde856633d284f5c47bf23f46ed2df2b0e96dd43
harizm837-dot <harizm837@gmail.com>
2026-02-09 09:18:27 +0800

    Delete js/chat.js

--- js/chat.js
+++
@@ -1,9 +0,0 @@
-function initChat(){
-  let c=document.getElementById("chat");
-  setInterval(()=>{
-    let m=document.createElement("div");
-    m.innerText=["🔥 Nice","😂 LOL","💎 VIP here","🎁 Gift!"][Math.floor(Math.random()*4)];
-    c.appendChild(m);
-    c.scrollTop=c.scrollHeight;
-  },1500);
-}
